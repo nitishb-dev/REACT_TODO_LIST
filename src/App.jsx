7465f1e0c2f355a33e41a86b8c1be37ff6295350
NITISH B <nitishb057@gmail.com>
2025-02-24 22:50:02 +0530

    todo created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Todo from "./components/Todo";
+ 
+const App = () => {
+  return (
+    <div className="bg-gray-950 flex justify-center items-center min-h-screen p-4">
+      <Todo/>
+    </div>
+  )
+}
+
+export default App;
